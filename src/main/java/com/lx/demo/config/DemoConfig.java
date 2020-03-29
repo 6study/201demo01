5c5dd4384d56dc03a99b54e0a480851fb530a040
6study <940180744@qq.com>
2020-03-29 17:17:42 +0800

    fix:(201) 配置类提交（#1）

---
+++ src/main/java/com/lx/demo/config/DemoConfig.java
@@ -0,0 +1,14 @@
+package com.lx.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Demo的配置类
+ *
+ * @author LiuXue on 2020/2/20
+ */
+@Configuration
+public class DemoConfig {
+
+
+}
